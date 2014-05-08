152bec174171b0233fc26841c83b89f7f17d26f1
shanxu <kingundertree@163.com>
2014-05-08 09:36:03 +0800

    add project

---
+++ XXHeaderRefresh/XXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXAppDelegate.h
+//  XXHeaderRefresh
+//
+//  Created by xiazer on 14-5-8.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XXHeaderRefresh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XXHeaderRefresh
+//
+//  Created by xiazer on 14-5-8.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XXAppDelegate class]));
+    }
+}
